8e8c0d3b7aee034a315c35016cc1a3281465bbbf
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-09-13 19:38:36 -0300

    practice loops

--- .learn/resets/02.4-One_last_looping/app.py
+++ .learn/resets/02.4-One_last_looping/app.py
@@ -1,9 +1,3 @@
 names = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fernando','Cesco','Bart','Annie']
 
 # Your code here
-names[1] = 'Steven'
-names[-1] = 'Pepe'
-names[0] = str(names[2] + names[4])
-
-for i in names[::-1]:
-    print(i)
--- exercises/01.5-Add-item-to-list/app.py
+++ exercises/01.5-Add-item-to-list/app.py
@@ -1,5 +1,10 @@
 # Remember to import random function here
+import random
 
 my_list = [4, 5, 734, 43, 45]
 
 # The magic goes below
+
+for i in range(1,11):
+    my_list.append(random.randint(1,100))
+    print(my_list)
