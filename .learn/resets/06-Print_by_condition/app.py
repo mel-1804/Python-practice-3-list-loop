7c032e90c7755ad2f67e4b917d4f59efb5a8f837
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-09-16 18:43:21 -0300

    practice list&loops

--- .learn/resets/06-Print_by_condition/app.py
+++ .learn/resets/06-Print_by_condition/app.py
@@ -2,8 +2,5 @@ my_list = [3344,34334,454543,342534,4563456,3445,23455,234,262,2335,43323,4356,3
 
 
 for i in my_list:
-    if i % 14 == 0:
-        print(i)
-
-   
+    # The magic happens here
     
--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -1,3 +1,12 @@
 my_list = [4,5,734,43,45,100,4,56,23,67,23,58,45]
 
 # Your code here
+def sum_all_values(list):
+    total = 0
+    for i in my_list:
+        if i % 2 != 0:
+            total += i
+    
+    return total
+    
+print(sum_all_values(my_list))
