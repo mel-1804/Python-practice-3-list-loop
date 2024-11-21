512d310f04bc5b667f9e45f556560da12daba61c
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-11-20 23:24:20 -0300

    avance ejercico 12.5

--- exercises/12.5-Yes_and_no/app.py
+++ exercises/12.5-Yes_and_no/app.py
@@ -2,3 +2,14 @@ the_bools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1]
 
 # Your code here
 
+def convertion_func():
+    if i == 1: 
+        print('wiki')
+    else:
+        print('woko')
+
+new_list = list(map(convertion_func(), the_bools))
+# new_list = list(map(lambda x:' wiki' if x == 1 else 'woko', the_bools))
+print(new_list)
+
+
